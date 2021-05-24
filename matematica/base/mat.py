c5a35be447c86241ed5b42b6e9e884fa18735a67
Nadia Ligia <nlnadialigia@gmail.com>
2021-05-24 12:37:05 -0300

    feat: Conceitos Paradigmas Procedural

---
+++ matematica/base/mat.py
@@ -0,0 +1,16 @@
+"""Módulo que contém operações Matemáticas"""
+
+
+def soma(parcela, parcela2):
+    """
+    Essa função calcula a soma de duas parcelas
+    :param parcela: number
+    :param parcela2: number
+    :return: number
+    """
+    return parcela + parcela2
+
+
+if __name__ == '__main__':
+    # Testando a função soma
+    print(soma(1, 2))  # imprimindo
